672626f1c2209544801c77616072b284dd13f115
lafierro5 <77758839+lafierro5@users.noreply.github.com>
2025-09-23 14:43:41 -0600

    Update and rename Column.java to Columns.java

---
+++ Columns.java
@@ -0,0 +1,42 @@
+public enum Columns {
+	A('A', 1), B('B', 2), C('C', 3), 
+	D('D', 4), E('E', 5), F('F', 6),
+	G('G', 7), H('H', 8);
+	
+	private final char column;
+	private final int index;
+	
+	private Columns(char column, int index) {
+		this.column = column;
+		this.index = index;
+	}
+	
+	public char getColumn() {
+		return this.column;
+	}
+	public int getIndex() {
+		return this.index;
+	}
+	
+	public boolean inColumns(char column) {
+		for(Columns columns : Columns.values()) {
+			if(columns == Columns.valueOf(String.valueOf(column).toUpperCase())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		String columns = "";
+		for(Columns column : Columns.values()) {
+			if(column != H) {
+				columns += column.getColumn() + ", ";
+			}
+			else {
+				columns += column.getColumn();
+			}
+		}
+		return columns;
+	}
