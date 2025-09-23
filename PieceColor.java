21a874bf18db64a3507ca08f861fe794e6bbf320
lafierro5 <77758839+lafierro5@users.noreply.github.com>
2025-09-23 14:41:33 -0600

    Colors.java

--- PieceColor.java
+++ PieceColor.java
@@ -1,1 +1,20 @@
 
+public enum Colors {
+	BLACK("Black"), WHITE("White");
+	
+	private final String color;
+	
+	private Colors(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	@Override
+	public String toString() {
+		return BLACK.getColor() + " or " + WHITE.getColor();
+	}
+}
+
