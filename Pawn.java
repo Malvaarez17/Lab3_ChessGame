57a366fcde81e26485ae3e6c38003bd6f3f81818
Malvaarez17 <maalvarez17@miners.utep.edu>
2025-09-24 16:54:10 -0600

    Columns Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,1 +1,30 @@
-
+public class Pawn extends ChessPiece {
+    public Pawn() {
+        super();
+    }
+    public Pawn(Colors color, Columns currentX, int currentY) {
+        super("Pawn", color, currentX, currentY);
+    }
+    public boolean validMove(Columns targetX, int targetY) {
+        if (!Chessboard.bounds(targetX, targetY)) {
+            return false;
+        }
+        if (currentX.getIndex() == targetX.getIndex()) {
+            if (color == Colors.BLACK) {
+                if (targetY - currentY == -1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (targetY - currentY == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+}
