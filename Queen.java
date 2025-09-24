225b8e709e2aba50326ea1f5eae7b500371742c0
Malvaarez17 <maalvarez17@miners.utep.edu>
2025-09-24 16:55:22 -0600

    Columns Queen.java

--- Queen.java
+++ Queen.java
@@ -1,1 +1,27 @@
-
+public class Queen extends Bishop {
+    public Queen() {
+        super();
+    }
+    public Queen(Colors color, Columns currentX, int currentY) {
+        super(color, currentX, currentY);
+        this.name = "Queen"; 
+    }
+    public boolean validMove(Columns targetX, int targetY) {
+        if (!Chessboard.bounds(targetX, targetY)) {
+            return false;
+        }
+        int deltaX = Math.abs(targetX.getIndex() - currentX.getIndex());
+        int deltaY = Math.abs(targetY - currentY);
+        if (currentX.getIndex() == targetX.getIndex() && currentY != targetY) {
+            return true;
+        }
+        if (currentY == targetY && currentX.getIndex() != targetX.getIndex()) {
+            return true;
+        }
+        if (deltaX == deltaY && deltaX > 0) {
+            return true;
+        }
+        
+        return false;
+    }
+}
