318bb1b9ebf86f9dc74152b2b8de6e7bfe38a590
Malvaarez17 <maalvarez17@miners.utep.edu>
2025-09-24 16:55:44 -0600

    Columns Rook.java

--- Rook.java
+++ Rook.java
@@ -1,1 +1,19 @@
+public class Rook extends ChessPiece {
 
+    public Rook() {
+        super();
+    }
+    public Rook(Colors color, Columns currentX, int currentY) {
+        super("Rook", color, currentX, currentY);
+    }
+    public boolean validMove(Columns targetX, int targetY) {
+        if (!Chessboard.bounds(targetX, targetY)) {
+            return false;
+        }
+        if (currentX.getIndex() == targetX.getIndex() || currentY == targetY) {
+            return true;
+        }
+        
+        return false;
+    }
+}
