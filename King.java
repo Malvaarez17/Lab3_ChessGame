878cb78de12ac77f581ecc107356d4794b6a53f7
Malvaarez17 <maalvarez17@miners.utep.edu>
2025-09-24 16:53:25 -0600

    Update King.java
    Changed to use Columns

--- King.java
+++ King.java
@@ -1,1 +1,19 @@
-
+public class King extends Queen {
+    public King() {
+        super();
+    }
+    public King(Colors color, Columns currentX, int currentY) {
+        super(color, currentX, currentY);
+        this.name = "King"; 
+    }
+    public boolean validMove(Columns targetX, int targetY) {
+        int dx = Math.abs(targetX.getIndex() - currentX.getIndex());
+        int dy = Math.abs(targetY - currentY);
+        if (Chessboard.bounds(targetX, targetY)) {
+            if (dx <= 1 && dy <= 1 && !(dx == 0 && dy == 0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
