394f7cda425ae8a99a284b656c85183bce6c8a01
Malvaarez17 <maalvarez17@miners.utep.edu>
2025-09-24 16:47:05 -0600

    Update Chessboard.java
    Updated Chessboard with Colums

--- Chessboard.java
+++ Chessboard.java
@@ -1,1 +1,22 @@
 
+public class Chessboard {
+
+    public static final int MIN_ROW = 1;
+    public static final int MAX_ROW = 8;
+
+
+    public static boolean bounds(Columns x, int y) {
+        if (x == null){ 
+            System.out.println("Invalid X: " + x + ". It must be between A and H.");
+            return false;
+        }
+
+        if (y < MIN_ROW || y > MAX_ROW){ 
+            System.out.println("Invalid Y: " + y +".  Is invlaid please choose between 1 and 8");
+            return false;
+        }
+
+        return true;
+    }
+    
+}
